4dcdf56d39db2758f23b5b251a7633f5440025f7
Vitalie <vitaliedms@gmail.com>
2024-02-29 13:37:55 +0200

    Add projects/contributions_btns functionality

--- static/js/script.js
+++ static/js/script.js
@@ -5,4 +5,25 @@ function show_about() {
   } else {
     x.style.display = "none";
   }
+};
+function show_projects() {
+  var x = document.getElementById("projects");
+  var y = document.getElementById("contributions");
+  if (x.style.display === "none" || x.style.display === "") {
+    y.style.display = "none";
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+};
+
+function show_contributions() {
+  var x = document.getElementById("contributions");
+  var y = document.getElementById("projects");
+  if (x.style.display === "none" || x.style.display === "") {
+    y.style.display = "none";
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
