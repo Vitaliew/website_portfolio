c4eb4c66430a783c90df92dcc8089605f262f577
Vitalie <vitaliedms@gmail.com>
2024-02-29 13:25:34 +0200

    Initial side-panel commit

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ import requests
 app = Flask(__name__)
 
 projects_list = []
+contributions_list = []
 for i in range(12):
     projects_list.append(f"Project {i}")
+    contributions_list.append(f"Contribution {i}")
+
 
 github_link = "https://github.com/Vitaliew"
 
 
 @app.route("/")
 def home():
-    return render_template("index.html", projects=projects_list, github_link=github_link)
+    return render_template("index.html", contributions=contributions_list, projects=projects_list, github_link=github_link)
 
 
 if __name__ == "__main__":
