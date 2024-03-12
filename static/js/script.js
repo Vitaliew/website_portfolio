5a2ad8e66d4fb9a54a0540345fb4cb2a34365b58
Vitalie <vitaliedms@gmail.com>
2024-03-12 07:55:13 +0200

    Improve js code so that there is only one function for clicking panel buttons

--- static/js/script.js
+++ static/js/script.js
@@ -13,25 +13,21 @@ function show_about() {
   }
 };
 
-var i = 0;
-function show_projects() {
-    x = document.getElementById("projects");
-    y = document.getElementById("contributions");
-    button_x = document.getElementById("projects_btn");
-    button_y = document.getElementById("contributions_btn");
-    button_x.style.color = "#0B60B0";
+function show_panel(clicked_id) {
+    if (clicked_id === "projects_btn") {
+        button_x = document.getElementById(clicked_id);
+        button_y = document.getElementById("contributions_btn");
+        x = document.getElementById("projects");
+        y = document.getElementById("contributions");
+    }
+    else if (clicked_id === "contributions_btn") {
+        button_x = document.getElementById(clicked_id);
+        button_y = document.getElementById("projects_btn");
+        x = document.getElementById("contributions");
+        y = document.getElementById("projects");
+    }
     button_y.style.color = "white";
-    y.style.width = "0%";
-    x.style.width = "25%";
-};
-
-function show_contributions() {
-    x = document.getElementById("contributions");
-    y = document.getElementById("projects");
-    button_x = document.getElementById("contributions_btn");
-    button_y = document.getElementById("projects_btn");
     button_x.style.color = "#0B60B0";
-    button_y.style.color = "white";
     y.style.width = "0%";
     x.style.width = "25%";
 };
