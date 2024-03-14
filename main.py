07c53c6f44df0d32e7197b764091c863e71fae2b
Vitalie <vitaliedms@gmail.com>
2024-03-14 19:23:49 +0200

    Add functionality to projects tab so that it shows all github portfolio projects

--- main.py
+++ main.py
@@ -1,26 +1,41 @@
 from flask import Flask, render_template, request
 import requests
+from bs4 import BeautifulSoup
+
 
 app = Flask(__name__)
-
-projects_list = []
-contributions_list = []
-for i in range(25):
-    projects_list.append(f"Project {i}")
-    contributions_list.append(f"Contribution {i}")
-
-
-github_link = "https://github.com/Vitaliew"
-about_text = ("I am a junior python developer. I have completed a python bootcamp, and now I am working"
-              " on my portfolio.")
-achievements_dict = {"CS50": "https://certificates.cs50.io/19393c00-a2d1-49f2-b57d-e49e47007b5c.png?size=letter",
-                     "PYTHON BOOTCAMP": ""}
 
 
 @app.route("/")
 def home():
-    return render_template("index.html", contributions=contributions_list,
-                           projects=projects_list, github_link=github_link, about=about_text,
+    github_link = "https://github.com/Vitaliew"
+    about_text = ("I am a junior python developer. I have completed a python bootcamp, and now I am working"
+                  " on side projects.")
+    achievements_dict = {"CS50": "https://certificates.cs50.io/19393c00-a2d1-49f2-b57d-e49e47007b5c.png?size=letter",
+                         "PYTHON BOOTCAMP": ""}
+    github_projects_dict = {}
+    github_contributions_dict = {}
+
+    response = requests.get("https://github.com/stars/Vitaliew/lists/portofolio")
+    status_code = response.status_code
+    if status_code != 200:
+        github_projects_dict["<p style='color:red;' > GITHUB ERROR </p>"] = ""
+        github_contributions_dict["<p style='color:red;' > GITHUB ERROR </p>"] = ""
+    else:
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        github_projects_list = soup.find(id="user-list-repositories")
+        github_projects_h3 = github_projects_list.find_all("h3")
+        for h3 in github_projects_h3:
+            href = f"https://github.com{h3.find_next('a').get('href')}"
+            name = href.split("/")
+            github_projects_dict[name[-1]] = href
+
+        for i in range(25):
+            github_contributions_dict[f"Contribution {i}"] = ""
+
+    return render_template("index.html", contributions=github_contributions_dict,
+                           projects=github_projects_dict, github_link=github_link, about=about_text,
                            achievements=achievements_dict)
 
 
